77a3e9aa0b2f5e038a5bc52bf87b54cc9ecd08ab
paulo <paulosilvajr.dev@gmail.com>
2022-05-09 21:44:07 -0300

    Adicionando comportamentos a classe MainActivity testando metodos de atribuição a views.

--- app/src/main/java/alura/com/br/agenda/MainActivity.java
+++ app/src/main/java/alura/com/br/agenda/MainActivity.java
@@ -3,17 +3,33 @@ package alura.com.br.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends Activity {
+    //Sobrescrevendo metodo onCreate da class Active.
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Paulo Da Silva");
-        setContentView(aluno);
+
+        //chamando o método setContentView() enviando a referência do arquivo estático de layout criado.
+        setContentView(R.layout.activity_main);
+
+        //Ciando uma lista que ira representar os alunos implementando um ArrayList para isto.
+        List<String> alunos = new ArrayList<>(Arrays.asList("Paulo", "Isabela", "Alex"));
+
+        //obtento as views para vincular os dados, usando o metodo findViewById para identificar as views pelo seu Id.
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+
+        //vinculando as views com a lista,
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
